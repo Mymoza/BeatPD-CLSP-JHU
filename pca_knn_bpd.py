17e306d93d31bd74d7bbc4d7ecab54f576b424b8
Mymoza <mariephilippe.gill@gmail.com>
2020-03-06 13:39:52 -0500

    Updated PCA KNN file

--- pca_knn_bpd.py
+++ pca_knn_bpd.py
@@ -10,40 +10,46 @@ import kaldi_io
 import argparse
 import pickle
 from sklearn.decomposition import PCA
+import pandas as pd
 
 # KNN
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
 
-import pandas as pd
-
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn import decomposition
-
+# MSE 
+from sklearn.metrics import mean_squared_error
 
 def pca(sFileTrai, sFileTest, iComponents):
-    # os.chdir("/export/c10/lmorove1/kaldi/egs/xVecPD/v2_NeuroConc16k")
-    # sFileTrai="/export/c10/lmorove1/kaldi/egs/xVecPD/v2_NeuroConc16k/exp/sre18nn/xvectors_Training_Fold1/xvector.scp"
-    # sOut = Folder where to store results
-    # iComponents = nb of classes (nb of measurements_id? or nb of subject_ids?)
+    """
+    Performs PCA 
+    
+    Keyword arguments: 
+    - sFileTrai: TODO
+    - sFileTest: TODO
+    - iComponents: No of components to perform 
+    
+    Returns: 
+    - vTraiPCA: Training data transformed by PCA for all subject_id 
+    - vLTrai: Training labels for all subject_id 
+    - vTraiSubjectId: List of the subject_id for training 
+    - vTestPCA: Testing data transformed by PCA for all subject_id 
+    - vLTest: Test labels for all subject_id 
+    - vTestSubjectId: List of the subject_id for test
+    """
     dIvecTrai = { key:mat for key,mat in kaldi_io.read_vec_flt_scp(sFileTrai) }
     vTrai= pd.DataFrame((list(dIvecTrai.values())))
     vLTrai = np.array([x[-1] for x in np.array(list(dIvecTrai.keys()))])
-        
+
     # FIXME : For realPD, we need more than -5 (CIS-PD subject_id is 4 characters long)
-    # FIXME REAL-PD it's not only int
+    # FIXME REAL-PD it's not only int 
     vTraiSubjectId = np.array(([int(x[-5:-1]) for x in np.array(list(dIvecTrai.keys()))]))
-    
+
     dIvecTest = { key:mat for key,mat in kaldi_io.read_vec_flt_scp(sFileTest) }
     vTest=np.array(list(dIvecTest.values()), dtype=float)
     vLTest=np.array([int(x[-1]) for x in np.array(list(dIvecTest.keys()))])
     vTestSubjectId = np.array([int(x[-5:-1]) for x in np.array(list(dIvecTest.keys()))])
 
     #iComponents=60;
-    
+
     if isinstance(iComponents, str):
         iComponents=int(iComponents)
         
@@ -53,12 +59,19 @@ def pca(sFileTrai, sFileTest, iComponents):
     vTraiPCA=pca.transform(vTrai)
     vTestPCA=pca.transform(vTest)
 
-    #print('after transform : ', vTraiPCA.shape)
-    #print('after transform : ', vTestPCA.shape)
     return vTraiPCA, vLTrai, vTraiSubjectId, vTestPCA, vLTest, vTestSubjectId
 
-def pca_plda_bpd(sFileTrai, sFileTest, sOut, iComponents):
-
+def pca_knn_bpd(sFileTrai, sFileTest, sOut, iComponents, iNeighbors):
+    """
+    Performs PCA, then KNN and dumps the results in a pickle file 
+    
+    Keyword arguments: 
+    - sFileTrai: TODO
+    - sFileTest: TODO
+    - sOut: TODO
+    - iComponents: TODO
+    - iNeighbors: TODO
+    """
     vTraiPCA, vLTrai, vTraiSubjectId, vTestPCA, vLTest, vTestSubjectId = pca(sFileTrai, sFileTest, iComponents)
 
     # To compute the mean accuracy
@@ -66,24 +79,14 @@ def pca_plda_bpd(sFileTrai, sFileTest, sOut, iComponents):
     glob_test_pred=[]
     glob_trai_true=[]
     glob_test_true=[]
+    
+    # To compute the final score as per the challenge
+    mse_training_per_subjectid=[]
+    mse_test_per_subjectid=[]
+    train_nb_files_per_subjectid=[]
+    test_nb_files_per_subjectid=[]
 
     for subject_id in np.unique(vTraiSubjectId):
-        # GridSearchCV
-    #     parameters = {'n_neighbors':[1,3,6,10,15]}
-
-        k_range = list(range(1, 15))
-    #     param_grid = dict(n_neighbors=k_range)
-    #     print(param_grid)
-
-        # instantiate the grid
-    #     grid = GridSearchCV(knn, param_grid, cv=10, scoring='accuracy')
-
-        # fit the grid with data
-    #     grid.fit(vTraiPCA_subjectid, vLTrai_subjectid)
-
-        # view the complete results (list of named tuples)
-    #     print(grid.cv_results_['mean_test_score'])
-
         print('----- ' + str(subject_id) + '----- ')
         knn = KNeighborsClassifier(n_neighbors=6)
 
@@ -91,40 +94,30 @@ def pca_plda_bpd(sFileTrai, sFileTest, sOut, iComponents):
         indices_subject_id = np.where(vTraiSubjectId == subject_id) # HAPPY
         vTraiPCA_subjectid = vTraiPCA[indices_subject_id]
         vLTrai_subjectid = vLTrai[indices_subject_id]
+        vLTrai_subjectid = vLTrai_subjectid.astype(int)
 
         # Filter vTestPCA and vLTestPCA for one subject_id
         indices_subject_id = np.where(vTestSubjectId == subject_id)
         vTestPCA_subjectid = vTestPCA[indices_subject_id]
         vLTest_subjectid = vLTest[indices_subject_id]
-
+        vLTest_subjectid = vLTest_subjectid.astype(int)
+        
+        # We train the KNN only on the data for one subject_id 
         knn.fit(vTraiPCA_subjectid, vLTrai_subjectid)
-
-    #     y_labels = knn.predict(vTraiPCA_subjectid)
-    #     display(y_labels)
-
         print('Training accuracy: ', knn.score(vTraiPCA_subjectid, vLTrai_subjectid))
         print('Testing accuracy: ', knn.score(vTestPCA_subjectid, vLTest_subjectid))
-
-        # Predicting on the training and test data 
+        
+        # Predicting on the training and test data
         predictionsTrai = knn.predict(vTraiPCA_subjectid)
         predictions = knn.predict(vTestPCA_subjectid)
 
-        # Converting all strings to int for MSE 
-        vLTrai_subjectid = [int(i) for i in vLTrai_subjectid]
-        predictionsTrai = [int(i) for i in predictionsTrai]
-        vLTest_subjectid = [int(i) for i in vLTest_subjectid]
-        predictions = [int(i) for i in predictions]
-
-        # Computing the accuracy 
+        # Computing the accuracy
         glob_trai_pred=np.append(glob_trai_pred,predictionsTrai,axis=0)
         glob_test_pred=np.append(glob_test_pred,predictions,axis=0)
         glob_trai_true=np.append(glob_trai_true,vLTrai_subjectid,axis=0)
         glob_test_true=np.append(glob_test_true,vLTest_subjectid,axis=0)
 
-        a = [a - b for a, b in zip(vLTrai_subjectid, predictionsTrai)]
-        print('abs substract : ', np.abs(a))
-
-        # Building a list of the MSEk 
+        # Building a list of the MSEk
         mse_training_per_subjectid = np.append(mse_training_per_subjectid,
                                                (mean_squared_error(vLTrai_subjectid, predictionsTrai) /  len(vLTrai_subjectid)))
         mse_test_per_subjectid = np.append(mse_test_per_subjectid,
@@ -132,36 +125,34 @@ def pca_plda_bpd(sFileTrai, sFileTest, sOut, iComponents):
         train_nb_files_per_subjectid.append(len(vLTrai_subjectid))
         test_nb_files_per_subjectid.append(len(vLTest_subjectid))
 
-    #     do_confusion_matrix(vLTest_subjectid, predictions)
-
     print('Global training accuracy: {}'.format((glob_trai_true == glob_trai_pred).mean()))
     print('Global testing accuracy: {}'.format((glob_test_true == glob_test_pred).mean()))
     print('PCAComponents: {}'.format((iComponents)))
-   # print('True Labels Training:')
-   # print(glob_trai_true)
-   # print('#')
-   # print('#')
-   # print('Predicted Labels Training:')
-   # print(glob_trai_pred)
-   # print('#')
-   # print('#')
-   # print('True Labels Testing:')
-   # print(glob_test_true)
-   # print('#')
-   # print('#')
-   # print('Predicted Labels Testing:')
-   # print(glob_test_pred)
-   # print('#')
-   # print('#')
+    print('iNeighbors: {}'.format((iNeighbors)))
+    # print('True Labels Training:')
+    # print(glob_trai_true)
+    # print('#')
+    # print('#')
+    # print('Predicted Labels Training:')
+    # print(glob_trai_pred)
+    # print('#')
+    # print('#')
+    # print('True Labels Testing:')
+    # print(glob_test_true)
+    # print('#')
+    # print('#')
+    # print('Predicted Labels Testing:')
+    # print(glob_test_pred)
+    # print('#')
+    # print('#')
 
     if not  os.path.isdir(sOut):
             os.mkdir(sOut)
-    sObjname='objs_'+str(iComponents)+'.pkl'
+    sObjname='objs_'+str(iComponents)+'_k_'+str(iNeighbors)+'.pkl'
     with open(os.path.join(sOut,sObjname), 'wb') as f:  # Python 3: open(..., 'wb')
-        pickle.dump([glob_trai_pred,glob_trai_true,glob_test_pred,glob_test_true], f)
-
-def pca_plda_bpd(sFileTrai, sFileTest, sOut, iComponents):
-    
+        pickle.dump([glob_trai_pred,glob_trai_true,glob_test_pred,glob_test_true, \
+                    mse_training_per_subjectid,mse_test_per_subjectid, \
+                    train_nb_files_per_subjectid,test_nb_files_per_subjectid], f) 
 
 if __name__ == "__main__":
     # Usage example : 
@@ -177,8 +168,9 @@ if __name__ == "__main__":
     parser.add_argument('--input-test',dest='sFileTest', required=True)
     parser.add_argument('--output-file', dest='sOut', required=True)
     parser.add_argument('--iComponents', dest='iComponents', required=True)
+    parser.add_argument('--iNeighbors', dest='iNeighbors', required=True)
     
     args=parser.parse_args()
     
-    pca_plda_bpd(**vars(args))
+    pca_knn_bpd(**vars(args))
                                     
