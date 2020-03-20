40c0c7d1424781e6c864ed73884c1d012836cb9c
Mymoza <mariephilippe.gill@gmail.com>
2020-03-20 10:29:38 -0400

    Another script to run to get SVR pkl files

---
+++ local/pca_svr_bpd.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate BeatPD
+
+export PATH="$HOME/anaconda3/bin:$PATH"
+export PATH="./local:$PATH"
+
+sFileTrai=$1
+sFileTest=$2
+sOut=$3
+iComponents=$4
+sKernel=$5
+fCValue=$6
+fEpsilon=$7
+
+echo sssssssssssss
+echo $sOut
+echo sssssssssssssss
+
+filePath=/home/sjoshi/codes/python/BeatPD/code/
+
+cmd=/export/c08/lmorove1/kaldi/egs/beatPDivec/v1_autoenc/utils/run.pl
+$cmd $sOut/pca_${iComponents}_svr_${sKernel}_${fCValue}_${fEpsilon}Testx.log \
+     ${filePath}pca_knn_bpd.py --input-trai $sFileTrai \
+     --input-test $sFileTest \
+     --output-file $sOut \
+     --iComponents $iComponents \
+     --sKernel $sKernel \
+     --fCValue $fCValue \
+     --fEpsilon $fEpsilon
+conda deactivate
