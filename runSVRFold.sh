f80ba97b1d9a2535c710b53a455be025b29a2793
Mymoza <mariephilippe.gill@gmail.com>
2020-03-20 10:28:52 -0400

    Added bash script to get pkl files for SVR

---
+++ runSVRFold.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+chmod 755 /home/sjoshi/codes/python/BeatPD/code/run_auto.sh
+if [ -f path.sh ]; then . ./path.sh; fi
+
+eval "$(conda shell.bash hook)"
+conda activate BeatPD 
+
+sDirFeats=/export/b03/sbhati/PD/BeatPD/AE_feats
+#sOut=$expDir/ivec_${ivecDim}/resiVecKNN_Fold${foldN}
+
+ivecDim=50
+stage=5
+
+for ((iNumFold=0; iNumFold <=4 ; iNumFold++))  do
+	/home/sjoshi/codes/python/BeatPD/code/run_auto.sh $iNumFold $sDirFeats $ivecDim $stage || exit 1;
+done
+
+source deactivate
