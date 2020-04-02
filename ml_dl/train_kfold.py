c855a58c514a9b629920cf5c7b5310df9ca99042
Saurabh Bhati <saurabhati@Saurabhs-MacBook-Pro.local>
2020-04-02 15:27:35 -0400

    lstm per kfold

--- ml_dl/train_kfold.py
+++ ml_dl/train_kfold.py
@@ -27,7 +27,7 @@ sess = tf.Session()
 parser = argparse.ArgumentParser(description="Initial Experiment with Wavenet")
 parser.add_argument("--data_type",default="cis")
 parser.add_argument("--data_real_subtype",default="")
-parser.add_argument("--pid",default=1039,type=int)
+parser.add_argument("--pid",default=0,type=int)
 parser.add_argument("--KFind",default=2,type=int)
 parser.add_argument("--subtask",default="on_off",choices=['on_off','dyskinesia', 'tremor'])
 parser.add_argument("-uad","--use_ancillarydata",action="store_true")
@@ -71,8 +71,21 @@ train_data_path = data_dir + data_type + "-pd.training_data/" + data_real_subtyp
 if data_type == "cis":
     kfold_path = "/home/sjoshi/codes/python/BeatPD/data/BeatPD/cis-pd.training_data.k_fold_v3/"
 
-file_name = str(pid) + '_train_kfold_' + str(KFind) + '.csv' 
-df_train_label = pd.read_csv(kfold_path+file_name)
+if pid == 0:
+    pids = np.array([1004,1006,1007,1019,1020,1023,1032,1034,1038,1039,1043,1044,1046,1048,1049,1051])
+    df_train_label = pd.DataFrame()
+    for temp_pid in pids:
+        print(temp_pid)
+        file_name = str(temp_pid) + '_train_kfold_' + str(KFind) + '.csv'
+        temp = pd.read_csv(kfold_path+file_name)
+        df_train_label = pd.concat([df_train_label,temp],axis=0,ignore_index=True)
+    N = df_train_label.shape[0]
+    ind = np.random.permutation(N)
+    df_train_label = df_train_label.iloc[ind]
+    df_train_label = df_train_label.reset_index(drop=True)
+else:
+    file_name = str(pid) + '_train_kfold_' + str(KFind) + '.csv' 
+    df_train_label = pd.read_csv(kfold_path+file_name)
 
 if not params:
     params = {}
@@ -92,6 +105,15 @@ cleanParams = copy.copy(params)
 cleanParams['add_rotation'] = 'False'
 cleanParams['add_noise'] = 'False'
 
+if use_ancillarydata:
+    ancillary_data_path = data_dir + data_type + '-pd.ancillary_data/'
+    label_path_ancillary=data_dir+data_type+'-pd.data_labels/'+data_type.upper()+'-PD_Ancillary_Data_IDs_Labels.csv'
+    df_ancillary_label = pd.read_csv(label_path_ancillary)
+    anci_params = copy.copy(params)
+    anci_params['data_path'] = ancillary_data_path
+    anci_cleanParams = copy.copy(cleanParams)
+    anci_cleanParams['data_path'] = ancillary_data_path
+
 #model = load_model(load_weights_dir+'mlp_AE_'+str(use_ancillarydata)+'.h5')
 encoder = load_model(load_weights_dir+'mlp_encoder_uad_'+str(use_ancillarydata)+params_append_str+'_ld_'+str(latent_dim)+'.h5')
 #encoder = load_model(load_weights_dir+'mlp_encoder_uad_'+str(use_ancillarydata)+'_ld_'+str(latent_dim)+'.h5')
@@ -100,6 +122,34 @@ encoder = load_model(load_weights_dir+'mlp_encoder_uad_'+str(use_ancillarydata)+
 ## LSTM 
 
 AE_feats, labels, ind_selected = get_AE_feats(encoder,df_train_label,subtask,cleanParams)
+if use_ancillarydata:
+    acni_AE_feats, anci_labels, anci_ind_selected = get_AE_feats(encoder,df_ancillary_label,subtask,anci_cleanParams)
+    AE_feats = combine_feats(AE_feats,acni_AE_feats)
+    labels = np.concatenate((labels,anci_labels),axis=0)
+    del acni_AE_feats,anci_labels
+
+temp_X = AE_feats
+temp_Y = labels
+
+if params['add_noise'] =='True' or params['add_rotation'] == 'True':
+    for i in range(dataAugScale):
+        temp_AE_feats, temp_labels, temp_ind_selected = get_AE_feats(encoder,df_train_label,subtask,params)
+        if use_ancillarydata:
+            acni_AE_feats, anci_labels, anci_ind_selected = get_AE_feats(encoder,df_ancillary_label,subtask,anci_cleanParams)
+            temp_AE_feats = combine_feats(temp_AE_feats,acni_AE_feats)
+            temp_labels = np.concatenate((temp_labels,anci_labels),axis=0)
+            del acni_AE_feats,anci_labels,anci_ind_selected
+        temp_X = np.concatenate((temp_AE_feats,temp_X),axis=0)
+        temp_Y = np.concatenate((temp_labels,temp_Y),axis=0)
+        del temp_AE_feats, temp_labels, temp_ind_selected
+
+print("Original Size: %f" % (AE_feats.shape[0]))
+print("Augumented Size: %f" % (temp_X.shape[0]))
+
+N = temp_X.shape[0]
+ind = np.random.permutation(N)
+temp_X = temp_X[ind,:,:]
+temp_Y = temp_Y[ind,:]
 
 LSTM_featsize = AE_feats.shape[-1]
 classifier = make_LSTM_model(feat_size=LSTM_featsize)
@@ -110,36 +160,12 @@ lr=0.0001
 sgd = SGD(lr=lr, decay=0, momentum=0.9, nesterov=True)
 classifier.compile(optimizer='adam',loss='mse',metrics=['mae'])
 
-if warmstart_LSTM:
-    lstm_loadname = load_weights_dir + 'LSTM_uad_'+str(use_ancillarydata)+'_'+subtask+params_append_str+'_ld_'+str(latent_dim)+'.h5'
-    print(lstm_loadname)
-    classifier.load_weights(lstm_loadname)
-
-temp_X = AE_feats
-temp_Y = labels
-
-if params['add_noise'] =='True' or params['add_rotation'] == 'True':
-    for i in range(dataAugScale):
-        temp_AE_feats, temp_labels, temp_ind_selected = get_AE_feats(encoder,df_train_label,subtask,params)
-        temp_X = np.concatenate((temp_AE_feats,temp_X),axis=0)
-        temp_Y = np.concatenate((temp_labels,temp_Y),axis=0)
-        del temp_AE_feats, temp_labels, temp_ind_selected
-
-print("Original Size: %f" % (AE_feats.shape[0]))
-print("Augumented Size: %f" % (temp_X.shape[0]))
-
-#import pdb; pdb.set_trace()
-
-N = temp_X.shape[0]
-ind = np.random.permutation(N)
-temp_X = temp_X[ind,:,:]
-temp_Y = temp_Y[ind,:]
 #classifier.fit(temp_X,temp_Y,validation_split=0.10,batch_size=50,epochs=100,verbose=1,shuffle=True,callbacks=[early_stopping])
 classifier.fit(temp_X,temp_Y,batch_size=50,epochs=100,verbose=1,shuffle=True,callbacks=[early_stopping])
 
 del temp_X, temp_Y
 
-tr_pred = classifier.predict(AE_feats,batch_size=20)
+tr_pred = classifier.predict(AE_feats,batch_size=100,verbose=1)
 
 tr_class_weights = get_class_weights(df_train_label)
 
@@ -149,14 +175,26 @@ tr_w_mse = np.sum(temp[:,0]*tr_class_weights[ind_selected])/np.sum(tr_class_weig
 
 
 ## test 
-
-file_name = str(pid) + '_test_kfold_' + str(KFind) + '.csv' 
-df_test_label = pd.read_csv(kfold_path+file_name)
+if pid == 0:
+    pids = np.array([1004,1006,1007,1019,1020,1023,1032,1034,1038,1039,1043,1044,1046,1048,1049,1051])
+    df_test_label = pd.DataFrame()
+    for temp_pid in pids:
+        print(temp_pid)
+        file_name = str(temp_pid) + '_test_kfold_' + str(KFind) + '.csv'
+        temp = pd.read_csv(kfold_path+file_name)
+        df_test_label = pd.concat([df_test_label,temp],axis=0,ignore_index=True)
+    N = df_test_label.shape[0]
+    ind = np.random.permutation(N)
+    df_test_label = df_test_label.iloc[ind]
+    df_test_label = df_test_label.reset_index(drop=True)
+else:
+    file_name = str(pid) + '_test_kfold_' + str(KFind) + '.csv' 
+    df_test_label = pd.read_csv(kfold_path+file_name)
 
 
 test_AE_feats, test_labels, test_ind_selected = get_AE_feats(encoder,df_test_label,subtask,params)
 
-test_pred = classifier.predict(test_AE_feats,batch_size=20)
+test_pred = classifier.predict(test_AE_feats,batch_size=100,verbose=1)
 
 test_class_weights = get_class_weights(df_test_label)
 
