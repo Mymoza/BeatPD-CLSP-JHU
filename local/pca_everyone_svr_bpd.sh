647d33a1af01082641c5d413e9969d7a9c23ee70
Mymoza <mariephilippe.gill@gmail.com>
2020-04-16 11:41:23 -0400

    Small changes on master branch made them on this branch instead

--- local/pca_everyone_svr_bpd.sh
+++ local/pca_everyone_svr_bpd.sh
@@ -21,8 +21,11 @@ fCValueStr=$(printf "%g" $fCValue)
 #filePath=/home/mpgill/BeatPD/BeatPD-CLSP-JHU/
 
 # Path when the changes to run are merged on master
-filePath=/home/sjoshi/codes/python/BeatPD/code/
+#filePath=/home/sjoshi/codes/python/BeatPD/code/
 
+filePath=`pwd`/
+
+# BUG? might have to remove that big path? 
 cmd=/export/c08/lmorove1/kaldi/egs/beatPDivec/v1_autoenc/utils/run.pl
 $cmd $sOut/pca_${iComponents}_everyone_svr_${sKernel}_${fCValueStr}_${fEpsilon}Testx.log \
      pca_everyone_svr_bpd.py --input-trai $sFileTrai \
