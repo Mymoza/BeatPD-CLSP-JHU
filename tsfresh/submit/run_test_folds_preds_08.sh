a0816ceab1d53b830a5d7b85f5c0155f3e118152
Mymoza <mariephilippe.gill@gmail.com>
2021-02-03 11:22:03 -0500

    Updated the run sh files for approach 1

--- tsfresh/submit/run_test_folds_preds_08.sh
+++ tsfresh/submit/run_test_folds_preds_08.sh
@@ -9,8 +9,6 @@ conda activate BeatPD_xgboost
 path_labels_cis=/home/sjoshi/codes/python/BeatPD/data/BeatPD/cis-pd.data_labels/
 path_labels_real=/home/sjoshi/codes/python/BeatPD/data/BeatPD/real-pd.data_labels/
 
-
-
 # echo "TREMOR"
 # Everyone has the same parameters? 
 
@@ -20,9 +18,52 @@ path_labels_real=/home/sjoshi/codes/python/BeatPD/data/BeatPD/real-pd.data_label
 # python src/gridsearch.py dyskinesia features/cis-pd.training.csv ${path_labels_cis}/CIS-PD_Training_Data_IDs_Labels.csv 0
 
 
-python src/gridsearch.py on_off features/cis-pd.training.csv ${path_labels_cis}/CIS-PD_Training_Data_IDs_Labels.csv 0.8
-python src/gridsearch.py tremor features/cis-pd.training.csv ${path_labels_cis}/CIS-PD_Training_Data_IDs_Labels.csv 0.8
-python src/gridsearch.py dyskinesia features/cis-pd.training.csv ${path_labels_cis}/CIS-PD_Training_Data_IDs_Labels.csv 0.8
+python src/gridsearch.py  --features "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/features/cis-pd.training.csv" \
+                            --labels "/home/sjoshi/codes/python/BeatPD/data/BeatPD/cis-pd.data_labels/CIS-PD_Training_Data_IDs_Labels.csv" \
+                            --pred_path "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/linear_comb/" \
+                            --linear_combination 0.2
+
+python src/gridsearch.py tremor  --features "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/features/cis-pd.training.csv" \
+                                 --labels "/home/sjoshi/codes/python/BeatPD/data/BeatPD/cis-pd.data_labels/CIS-PD_Training_Data_IDs_Labels.csv" \
+                                 --pred_path "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/linear_comb/" \
+                                 --linear_combination 0.2
+
+python src/gridsearch.py dyskinesia  --features "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/features/cis-pd.training.csv" \
+                                     --labels "/home/sjoshi/codes/python/BeatPD/data/BeatPD/cis-pd.data_labels/CIS-PD_Training_Data_IDs_Labels.csv" \
+                                     --pred_path "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/linear_comb/" \
+                                    --linear_combination 0.2
+
+
+python src/gridsearch.py  --features "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/features/cis-pd.training.csv" \
+                            --labels "/home/sjoshi/codes/python/BeatPD/data/BeatPD/cis-pd.data_labels/CIS-PD_Training_Data_IDs_Labels.csv" \
+                            --pred_path "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/linear_comb/" \
+                            --linear_combination 0.4
+
+python src/gridsearch.py tremor  --features "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/features/cis-pd.training.csv" \
+                                 --labels "/home/sjoshi/codes/python/BeatPD/data/BeatPD/cis-pd.data_labels/CIS-PD_Training_Data_IDs_Labels.csv" \
+                                 --pred_path "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/linear_comb/" \
+                                 --linear_combination 0.4
+
+python src/gridsearch.py dyskinesia  --features "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/features/cis-pd.training.csv" \
+                                     --labels "/home/sjoshi/codes/python/BeatPD/data/BeatPD/cis-pd.data_labels/CIS-PD_Training_Data_IDs_Labels.csv" \
+                                     --pred_path "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/linear_comb/" \
+                                    --linear_combination 0.4
+
+
+python src/gridsearch.py  --features "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/features/cis-pd.training.csv" \
+                            --labels "/home/sjoshi/codes/python/BeatPD/data/BeatPD/cis-pd.data_labels/CIS-PD_Training_Data_IDs_Labels.csv" \
+                            --pred_path "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/linear_comb/" \
+                            --linear_combination 0.001
+
+python src/gridsearch.py tremor  --features "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/features/cis-pd.training.csv" \
+                                 --labels "/home/sjoshi/codes/python/BeatPD/data/BeatPD/cis-pd.data_labels/CIS-PD_Training_Data_IDs_Labels.csv" \
+                                 --pred_path "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/linear_comb/" \
+                                 --linear_combination 0.001
+
+python src/gridsearch.py dyskinesia  --features "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/features/cis-pd.training.csv" \
+                                     --labels "/home/sjoshi/codes/python/BeatPD/data/BeatPD/cis-pd.data_labels/CIS-PD_Training_Data_IDs_Labels.csv" \
+                                     --pred_path "/home/mpgill/BeatPD/BeatPD-CLSP-JHU/tsfresh/submit/linear_comb/" \
+                                    --linear_combination 0.001
 
 
 
